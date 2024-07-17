a87bc0b22eeef60a751e1609ee56faa31d7a6c85
yerm <leeyelim98@gmail.com>
2024-07-17 10:43:53 +0900

    20240717

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import styled from 'styled-components';
+
+const Header = styled.h1;
+
+function App() {
+  return (
+    <div className="App">
+      <Header>Hello</Header>
+    </div>
+  );
+}
+
+export default App;
