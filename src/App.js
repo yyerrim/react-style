3ddf7094da9af599715c7ceac879ff4a8dc61c2e
yerm <leeyelim98@gmail.com>
2024-07-17 10:54:42 +0900

    styled-component 사용 방법

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
 import styled from 'styled-components';
 
-const Header = styled.h1;
+// 자동완성이 되지 않는 아쉬움
+// const Header = styled.h1`
+//   color: red;
+//   text-decoration: underline;
+//   &:hover {
+//     color: yellow;
+//   }
+// `;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header>Hello</Header>
+//     </div>
+//   );
+// }
+
+const Header = styled.h1`
+  color: ${props => props.color};
+  text-decoration: underline;
+  transition: 3s;
+  &:hover {
+    color: yellow;
+    font-size: 2rem;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <Header>Hello</Header>
+      <Header color={'blue'}>Hello</Header>
     </div>
   );
 }
